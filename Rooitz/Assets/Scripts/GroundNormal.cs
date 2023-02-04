216ae0b61bc8c2323fcfe8ceb3d98b07b752a45a
Samba-Helina <henrikmoilanen@gmail.com>
2023-02-04 20:19:28 +0200

    Massapusku

---
+++ Rooitz/Assets/Scripts/GroundNormal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundNormal : MonoBehaviour
+{
+    // This is the normal we are giving
+    Vector2 groundNormal;
+    private GameObject groundObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundObject = gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Rooitz/Assets/Scripts/PlayerControl.cs
+++ Rooitz/Assets/Scripts/PlayerControl.cs
@@ -15,7 +15,7 @@ public class PlayerControl : MonoBehaviour
     // Object public variables
     public PlayerState activeState;
     public double minimumTilt = 0.1;
-    public float WalkingSpeed = 0.1f;
+    public float WalkingSpeed = 2;
 
     // This is used as a reference to see where character moves when controlled
     public Vector2 GroundNormal;
@@ -24,6 +24,8 @@ public class PlayerControl : MonoBehaviour
 
     // Object private variables/references
     private GameObject playerObject;
+    private Animator playerAnimator;
+    private Transform animatorTransform;
     private Rigidbody2D playerBody;
 
     // Start is called before the first frame update
@@ -32,7 +34,10 @@ public class PlayerControl : MonoBehaviour
         activeState = PlayerState.Standing;
         playerObject = gameObject;
         playerBody = (Rigidbody2D)playerObject.GetComponent("Rigidbody2D");
-        GroundNormal = new Vector2(1,0);
+        animatorTransform = playerObject.transform.GetChild(0);
+        playerAnimator = (Animator)playerObject.GetComponentInChildren(typeof(Animator));
+        // This might end up unneeded.
+        GroundNormal = new Vector2(1, 0);
     }
 
     // Update is called once per frame
@@ -47,14 +52,40 @@ public class PlayerControl : MonoBehaviour
             if (horizontal >= minimumTilt)
             {
                 activeState = PlayerState.Walking;
+                RotateCharacter(true);
+                playerAnimator.enabled = true;
+                playerAnimator.Play("Walking");
                 playerBody.velocity = Vector2.Scale(GroundNormal, new Vector2(WalkingSpeed, WalkingSpeed));
-            } else if (horizontal <= -minimumTilt) {
-                activeState = PlayerState.Walking;
-                playerBody.velocity = Vector2.Scale(GroundNormal, new Vector2(-WalkingSpeed, WalkingSpeed));
-            } else {
-                activeState = PlayerState.Standing;
-                playerBody.velocity = new Vector2(0,0);
             }
+            // Might be better withouth this left walking section
+            /*
+            else if (horizontal <= -minimumTilt)
+            {
+                activeState = PlayerState.Walking;
+                RotateCharacter(false);
+                playerAnimator.enabled = true;
+                playerAnimator.Play("Walking");
+                playerBody.velocity = Vector2.Scale(GroundNormal, new Vector2(-WalkingSpeed, WalkingSpeed));
+            }*/
+            else
+            {
+                playerAnimator.enabled = false;
+                activeState = PlayerState.Standing;
+                playerBody.velocity = new Vector2(0, 0);
+            }
+        }
+    }
+    void RotateCharacter(bool isFacingRight)
+    {
+        if (isFacingRight)
+        {
+            animatorTransform.localPosition = new Vector3(0.75f, 0, 0);
+            animatorTransform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            animatorTransform.localPosition = new Vector3(0.75f, 0, 0);
+            animatorTransform.localScale = new Vector3(-1, 1, 1);
         }
     }
 }
